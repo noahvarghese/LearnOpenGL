ce1fb2772bb3d1c8c2cf86b187a44fa3eb862c4b
noahvarghese <varghese.noah@gmail.com>
2022-02-28 00:57:06 +0000

    got first example working

--- src/examples/transformations.c
+++ src/examples/transformations.c
@@ -2,16 +2,21 @@
 
 int transformation_lesson(void)
 {
-    // vec4 vec = {1.0f, 0.0f, 0.0f, 1.0f};
+    vec4 vec = {1.0f, 0.0f, 0.0f, 1.0f};
 
     mat4 mat = {{1.0f, 0.0f, 0.0f, 0.0f},
                 {0.0f, 1.0f, 0.0f, 0.0f},
                 {0.0f, 0.0f, 1.0f, 0.0f},
                 {0.0f, 0.0f, 0.0f, 1.0f}};
 
+	glm_vec4_print(vec, stdout);
     glm_mat4_print(mat, stdout);
+
     glm_translate_make(mat, (vec3){1.0f, 1.0f, 0.0f});
+	glm_mat4_mulv(mat, vec, vec);
+
     glm_mat4_print(mat, stdout);
+	glm_vec4_print(vec, stdout);
 
     return 0;
 }
